039096e033a16c8f1d0fd4f81f16d81c82ae228b
stevector <steve@stevepersch.com>
2012-01-07 22:53:22 -0600

    Find and replace of state_flow and workbench_machine

--- modules/workbench_machine/workbench_machine.api.php
+++ modules/workbench_machine/workbench_machine.api.php
@@ -6,24 +6,24 @@
  */
 
 /**
- * Implements hook_state_flow_plugins().
+ * Implements hook_workbench_machine_plugins().
  *
  * Define the ctools plugin to add a new state machine type for the node workflow.
  * In this example we are Add a "reviewed" state to the StateFlow class.
  */
 
 /**
- * Implements hook_state_flow_plugins().
+ * Implements hook_workbench_machine_plugins().
  */
-function hook_state_flow_plugins() {
+function hook_workbench_machine_plugins() {
   $info = array();
-  $path = drupal_get_path('module', 'state_flow') . '/plugins';
-  $info['state_flow_test'] = array(
+  $path = drupal_get_path('module', 'workbench_machine') . '/plugins';
+  $info['workbench_machine_test'] = array(
     'handler' => array(
       'class' => 'StateFlowTest',
-      'file' => 'state_flow.inc',
+      'file' => 'workbench_machine.inc',
       'path' => $path,
-      'parent' => 'state_flow'
+      'parent' => 'workbench_machine'
     ),
   );
   return $info;
