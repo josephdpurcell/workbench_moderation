e79f3c79c787bd68d90439b8e1036a96bd4a5bf1
Steve Persch <persch@palantir.net>
2012-02-22 17:44:19 -0600

    Event plugin uses new tables

--- modules/workbench_events/plugins/export_ui/workbench_events_ui.class.php
+++ modules/workbench_events/plugins/export_ui/workbench_events_ui.class.php
@@ -1,6 +1,6 @@
 <?php
 
-module_load_include('php', 'workbench_states', 'plugins/export_ui/workbench_base_ui.class');
+module_load_include('php', 'workbench_moderation_exportables', 'plugins/export_ui/workbench_base_ui.class');
 
 class workbench_events_ui extends workbench_base_ui {
 
