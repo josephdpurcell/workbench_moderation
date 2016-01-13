68fb3ccf5156dc1129f0a77ef0ecbffe1cae432f
Ted Bowman <ted.git@tedbow.com>
2016-01-13 15:46:11 +0000

    Issue #2648308 by tedbow: Working update runner. Work around for core bug for Entity Query: https://www.drupal.org/node/2649268

--- src/Plugin/UpdateRunner/ModerationUpdateRunner.php
+++ src/Plugin/UpdateRunner/ModerationUpdateRunner.php
@@ -9,11 +9,8 @@
 namespace Drupal\workbench_moderation\Plugin\UpdateRunner;
 
 
-use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Entity\Query\QueryInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountSwitcherInterface;
 use Drupal\scheduled_updates\Plugin\BaseUpdateRunner;
 use Drupal\scheduled_updates\RevisionUtils;
@@ -21,7 +18,10 @@ use Drupal\workbench_moderation\ModerationInformationInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * The default Update Runner.
+ * The Workbench Moderation Update Runner.
+ *
+ * Works with Scheduled Updates module: https://www.drupal.org/project/scheduled_updates
+ *
  *
  * @UpdateRunner(
  *   id = "workbench_moderation",
@@ -36,11 +36,31 @@ class ModerationUpdateRunner extends BaseUpdateRunner{
    */
   protected $moderationInfo;
 
+  /**
+   * ModerationUpdateRunner constructor.
+   *
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $fieldManager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   * @param \Drupal\scheduled_updates\RevisionUtils $revisionUtils
+   * @param \Drupal\Core\Session\AccountSwitcherInterface $accountSwitcher
+   * @param \Drupal\workbench_moderation\ModerationInformationInterface $moderationInfo
+   */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityFieldManagerInterface $fieldManager, EntityTypeManagerInterface $entityTypeManager, RevisionUtils $revisionUtils, AccountSwitcherInterface $accountSwitcher, ModerationInformationInterface $moderationInfo) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $fieldManager, $entityTypeManager, $revisionUtils, $accountSwitcher);
     $this->moderationInfo = $moderationInfo;
   }
 
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration,
@@ -54,81 +74,47 @@ class ModerationUpdateRunner extends BaseUpdateRunner{
     );
   }
 
-
-
+  /**
+   * {@inheritdoc}
+   */
   protected function getReferencingUpdates() {
+    // @todo Logic for independent updates
     return [];
   }
 
-
-
-  public function getReferencedUpdates() {
-    $updates = [];
-    /** @var String[] $fields */
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIdsReferencingReadyUpdates() {
+    $entity_ids = [];
     if ($field_ids = $this->getReferencingFieldIds()) {
-
-      //$query = $entity_storage->getQuery('OR');
-      $entity_ids = [];
-
       $entity_storage = $this->entityTypeManager->getStorage($this->updateEntityType());
+      $all_ready_update_ids = $this->getReadyUpdateIds();
       foreach ($field_ids as $field_id) {
 
         $query = $entity_storage->getQuery('AND');
-        $this->addActiveUpdateConditions($query, "$field_id.entity.");
+        $query->condition("$field_id.target_id", $all_ready_update_ids, 'IN');
         $query->allRevisions();
         $entity_ids += $query->execute();
       }
-      /** @var ContentEntityInterface[] $entities */
-      //$entities = $entity_storage->loadMultiple(array_unique($entity_ids));
-      $revisions_to_update = [];
-      $entity_ids = array_unique($entity_ids);
-      foreach ($entity_ids as $entity_id) {
-        $revisions_to_update[$entity_id] = $this->moderationInfo->getLatestRevision($this->updateEntityType(), $entity_id);
-      }
-      /** @var ContentEntityInterface $revision */
-      foreach ($revisions_to_update as $entity_id => $revision) {
-        /** @var  $entity_update_ids - all update ids for this entity for our fields. */
-        $entity_update_ids = [];
-        /** @var  $field_update_ids - update ids keyed by field_id. */
-        $field_update_ids = [];
-        foreach ($field_ids as $field_id) {
-          // Store with field id.
-          $field_update_ids[$field_id] = $this->getEntityReferenceTargetIds($revision, $field_id);
-          // Add to all for entity.
-          $entity_update_ids += $field_update_ids[$field_id];
-        }
-        // For all entity updates return only those ready to run.
-        $ready_update_ids = $this->getReadyUpdates($entity_update_ids);
-        // Loop through updates attached to fields.
-        foreach ($field_update_ids as $field_id => $update_ids) {
-          // For updates attached to field get only those ready to run.
-          $field_ready_update_ids = array_intersect($update_ids, $ready_update_ids);
-          foreach ($field_ready_update_ids as $field_ready_update_id) {
-            $updates[] = [
-              'update_id' => $field_ready_update_id,
-              'entity_ids' => [$revision->id()],
-              //'revision_id' => $revision->getRevisionId(),
-              'field_id' => $field_id,
-              'entity_type' => $this->updateEntityType(),
-            ];
-          }
-        }
-      }
     }
-    debug($updates, $this->scheduled_update_type->id());
-    return $updates;
+    return $entity_ids;
   }
 
   /**
    * {@inheritdoc}
    */
   protected function loadEntitiesToUpdate($entity_ids) {
+    $revision_ids = array_keys($entity_ids);
+    $entity_ids = array_unique($entity_ids);
     $revisions = [];
     foreach ($entity_ids as $entity_id) {
-      $revisions[$entity_id] = $this->moderationInfo->getLatestRevision($this->updateEntityType(), $entity_id);
+      $latest_revision = $this->moderationInfo->getLatestRevision($this->updateEntityType(), $entity_id);
+      // Check the latest revision was in the revisions sent to this function.
+      if (in_array($latest_revision->getRevisionId(), $revision_ids)) {
+        $revisions[$entity_id] = $latest_revision;
+      }
     }
     return $revisions;
   }
-
-
 }
